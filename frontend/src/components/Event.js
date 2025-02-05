1d285b1d7805fa9c79f2da92d1e849c801aac5ba
AkilaPalaniappan-94 <akilapl94@gmail.com>
2025-02-05 18:36:27 -0500

    Follower Component added

--- frontend/src/components/Event.js
+++ frontend/src/components/Event.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Card} from 'react-bootstrap'
+import Follower from './Follower'
 
 
 
@@ -14,6 +15,7 @@ const Event = ({event}) => {
         <Card.Text>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'long', day: 'numeric' ,weekday:'long'}).format(new Date(event.date))}</Card.Text>
         {/*<Card.Text>{new Date(event.date).toLocaleDateString()}</Card.Text>*/}
         <Card.Text>{event.time}</Card.Text>
+        <Follower followers={event.followers}></Follower>
       </Card.Body>
     </Card> 
     

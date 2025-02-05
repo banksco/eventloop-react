303c41e181c75df839b3fae131ef9ced29ae2884
banksco <cdbankxx@gmail.com>
2025-02-04 20:34:07 -0600

    Header and Footer Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 const App = () => {
   return (
   <>
-<h1> Welcome to EventLoop</h1>
+    <Header />
+    <h1> Welcome to EventLoop</h1>
+    <Footer />
   </> 
   );
 }

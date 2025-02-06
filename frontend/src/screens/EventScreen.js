52a45fac3dbf78ade6059fb545363064da790ea1
banksco <cdbankxx@gmail.com>
2025-02-06 08:43:58 -0600

    productScreen update

--- frontend/src/screens/EventScreen.js
+++ frontend/src/screens/EventScreen.js
@@ -1,9 +1,51 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
+import {Row, Col, Image, Card, Button, ListGroup} from 'react-bootstrap'
+import Events from '../Events'
 
 const EventScreen = () => {
+  const params = useParams ()
+  const event = Events.find(p =>p.id === params.id)
+
+
   return (
-    <div>EventScreen</div>
+    <>
+    <Link className= 'btn btn-light my-3' to='/'>
+    Go Back
+   </Link>
+   <Row>
+    <Image src={event.image} alt='{event.name}' fluid />
+   </Row>
+   <Row>
+    <Col md={6}>
+    <Card>
+    <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>{event.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ${event.ticket_price}</ListGroup.Item>
+            <ListGroup.Item>Description: {event.description}</ListGroup.Item>
+          </ListGroup>
+          </Card>
+    </Col>
+
+    <Col md={6}>
+    <Card>
+    <Button
+                  className='btn-block'
+                  type='button'
+                  disabled={event.tickets_available === 0}
+                >
+                  Add To Cart
+                </Button>
+                </Card>
+    </Col>
+
+   </Row>
+
+
+
+   </>
   )
 }
 

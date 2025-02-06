135df7ad2240321f664f46cafb960dfd4622e37f
AkilaPalaniappan-94 <akilapl94@gmail.com>
2025-02-06 11:53:24 -0500

    Category Screen Added,Bootstrap theme changed,art image changed

--- frontend/src/components/Event.js
+++ frontend/src/components/Event.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Card} from 'react-bootstrap'
-
+import{Link} from 'react-router-dom'
+import Follower from './Follower'
 
 
 const Event = ({event}) => {
   return (
     <>
-    
+    <br></br>
     <Card>
       <Link to={`/event/${event.id}`}>
       <Card.Img variant="top" src={event.image}style={{ width: '100%', height: 'auto', objectFit: 'cover' ,display: 'block', margin: '0 auto' }}/>
@@ -15,7 +16,7 @@ const Event = ({event}) => {
       <Link to={`/event/${event.id}`}>
         <Card.Title>{event.title}</Card.Title>
         </Link>
-        <Card.Text>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'long', day: 'numeric' ,weekday:'long'}).format(new Date(event.date))}</Card.Text>
+        <Card.Text>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'long', day: 'numeric' ,weekday:'short'}).format(new Date(event.date))}</Card.Text>
         {/*<Card.Text>{new Date(event.date).toLocaleDateString()}</Card.Text>*/}
         <Card.Text>{event.time}</Card.Text>
         <Follower followers={event.followers}></Follower>

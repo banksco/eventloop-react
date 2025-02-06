df09fd6b78c537c82e23117484e9f0a2bb0ca856
banksco <cdbankxx@gmail.com>
2025-02-05 21:07:36 -0600

    card img edit, App.js Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,26 @@
 import Footer from "./components/Footer";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Header from "./components/Header";
-import {Container } from 'react-bootstrap';
+import {Container} from 'react-bootstrap';
 import HomeScreen from "./screens/HomeScreen";
+import EventScreen from "./screens/EventScreen";
 
 const App = () => {
   return (
   <>
+  <Router>
     <Header />
     <main>
       <Container>
-          <HomeScreen/>
+          <Routes>
+            <Route path="/" exact element={<HomeScreen />} />
+            <Route path="/event/:id" exact element={<EventScreen />} />
+          </Routes>
+          
       </Container>
     </main>
     <Footer />
+  </Router>
   </> 
   );
 }
--- frontend/src/components/Event.js
+++ frontend/src/components/Event.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {Card} from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import Events from '../Events'
 
 
 
@@ -7,10 +9,14 @@ const Event = ({event}) => {
   return (
     <>
     
-    <Card> 
-      <Card.Img variant="top" src={event.image}style={{ width: '300px', height: '200px', objectFit: 'cover' ,display: 'block', margin: '0 auto' }}/>
+    <Card>
+      <Link to={`/event/${event.id}`}>
+      <Card.Img variant="top" src={event.image}style={{ width: '100%', height: 'auto', objectFit: 'cover' ,display: 'block', margin: '0 auto' }}/>
+      </Link>
       <Card.Body>
+      <Link to={`/event/${event.id}`}>
         <Card.Title>{event.title}</Card.Title>
+        </Link>
         <Card.Text>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'long', day: 'numeric' ,weekday:'long'}).format(new Date(event.date))}</Card.Text>
         {/*<Card.Text>{new Date(event.date).toLocaleDateString()}</Card.Text>*/}
         <Card.Text>{event.time}</Card.Text>

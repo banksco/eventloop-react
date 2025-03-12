6caa7fcf2bd2474b34b13f57f0c7a1984f72760b
banksco <cdbankxx@gmail.com>
2025-03-12 16:25:04 -0500

    UserModel/ userControllers

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema=mongoose.Schema({
     name:{type:String,required:true},
@@ -6,6 +7,11 @@ const userSchema=mongoose.Schema({
     password:{type:String,required:true,minLength:5},
     isAdmin:{type:Boolean,required:true,default:false},
 },{timestamps:true})
+
+// matching entered pw with account pw
+userSchema.methods.matchPassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password)
+}
 
 const User=mongoose.model('users',userSchema)
 export default User
